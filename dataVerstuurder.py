8d615aecbfe70452752032a7a3619b55d73256fa
Mike Schouten <mikeschouten@Mikes-MacBook-Pro.local>
2023-05-11 12:30:32 +0200

    dataVerstuurder toegevoegd

--- dataVerstuurder.py
+++ dataVerstuurder.py
@@ -14,21 +14,21 @@ ser.timeout = 60  # 1 min
 ser.open()
 
 # thread function
-def threaded(c):
+# def threaded(c):
     
  
-        # data received from client
-    data = c.recv(1024)
-    if not data:
-        print('Bye')
-        # lock released on exit
-        print_lock.release()
-        return
-    my_json = data.decode('utf8')
-    printJson(my_json)
-    print_lock.release()
-    # connection closed
-    c.close()
+#         # data received from client
+#     data = c.recv(1024)
+#     if not data:
+#         print('Bye')
+#         # lock released on exit
+#         print_lock.release()
+#         return
+#     my_json = data.decode('utf8')
+#     printJson(my_json)
+#     print_lock.release()
+#     # connection closed
+#     c.close()
  
 def printJson(input):
     print('- ' * 20)
@@ -42,14 +42,14 @@ def Main():
     # reserve a port on your computer
     # in our case it is 12345 but it
     # can be anything
-    port = 12345
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("socket binded to port", port)
+    # port = 12345
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.bind((host, port))
+    # print("socket binded to port", port)
  
-    # put the socket into listening mode
-    s.listen(5)
-    print("socket is listening")
+    # # put the socket into listening mode
+    # s.listen(5)
+    # print("socket is listening")
  
     # a forever loop until client wants to exit
     while True:
@@ -58,12 +58,12 @@ def Main():
             x = ser.readline().decode('utf-8')
             printJson(x)
         # establish connection with client
-        c, addr = s.accept()
+        # c, addr = s.accept()
  
-        # lock acquired by client
-        print_lock.acquire() 
-        # Start a new thread and return its identifier
-        start_new_thread(threaded, (c,))
+        # # lock acquired by client
+        # print_lock.acquire() 
+        # # Start a new thread and return its identifier
+        # start_new_thread(threaded, (c,))
     s.close()
  
  
