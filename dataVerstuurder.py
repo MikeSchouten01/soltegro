9e029c43bd19480c60c47ab62ca79583bceffc22
Mike Schouten <mikeschouten@Mikes-MacBook-Pro.local>
2023-05-11 12:47:41 +0200

    dataVerstuurder toegevoegd

--- dataVerstuurder.py
+++ dataVerstuurder.py
@@ -14,65 +14,45 @@ ser.timeout = 60  # 1 min
 ser.open()
 
 # thread function
-def threaded(c):
-    
- 
-        # data received from client
+def receiveData(c):
     data = c.recv(1024)
     if not data:
         print('Bye')
-        # lock released on exit
         print_lock.release()
         return
-    my_json = data.decode('utf8')
-    printJson(my_json)
+    dataString = data.decode('utf8')
+    printJson(dataString)
     print_lock.release()
     # connection closed
     c.close()
  
 def printJson(input):
     print('- ' * 20)
-    data = json.loads(input)
-    s = json.dumps(data, indent=4, sort_keys=True)
-    print(s)
+    jsonData = json.loads(input)
+    jsonString = json.dumps(jsonData, indent=4, sort_keys=True)
+    print(jsonString)
 
 def checkSerial():
     while True:
         if ser.in_waiting > 0:
             print("test")
-            x = ser.readline().decode('utf-8')
-            printJson(x)
+            dataString = ser.readline().decode('utf-8')
+            printJson(dataString)
 
 def Main():
-    host = ""
- 
-    # reserve a port on your computer
-    # in our case it is 12345 but it
-    # can be anything
     start_new_thread(checkSerial, ())
+    host = ""
     port = 12345
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     print("socket binded to port", port)
- 
-    # put the socket into listening mode
     s.listen(5)
     print("socket is listening")
- 
-    # a forever loop until client wants to exit
+
     while True:
-        # if ser.in_waiting > 0:
-        #     print("test")
-        #     x = ser.readline().decode('utf-8')
-        #     printJson(x)
-        # establish connection with client
         c, addr = s.accept()
- 
-        # lock acquired by client
         print_lock.acquire() 
-        # Start a new thread and return its identifier
-        start_new_thread(threaded, (c,))
-    s.close()
+        start_new_thread(receiveData, (c,))
  
  
 if __name__ == '__main__':
